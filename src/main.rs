3882f257a81e34c3a5185156cd0ae7bceac57ffa
mTvare6 <mTvare6@users.noreply.github.com>
2021-08-14 09:04:18 +0530

    fix: locale

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use r_i18n::I18nConfig;
 use r_i18n::I18n;
 
 fn main() {
-    let config: I18nConfig = I18nConfig{locales: &["bg", "de", "en", "es", "fr",  "ie", "jp", "pl", "pt", "ru"], directory: "translations"};
+    let config: I18nConfig = I18nConfig{locales: &["en", "bg", "de", "es", "fr",  "ie", "jp", "pl", "pt", "ru"], directory: "translations"};
     let r_i18n: I18n = I18n::configure(&config);
     unsafe { 
         println!("{}", r_i18n.t("msg")); 
